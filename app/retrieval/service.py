15ec66a717d71e8d131b17c0fa99659b5f9365a0
sachin-source <sachinmgvt@gmail.com>
2025-12-25 14:15:33 +0530

    Retrieval service update to retrieve

---
+++ app/retrieval/service.py
@@ -0,0 +1,59 @@
+from typing import List
+
+from app.embeddings.embedder import Embedder
+from app.vectorstore.base import VectorStore
+
+class RetrievalService:
+    def __init__(self, vector_store:VectorStore, embedder:Embedder):
+        self.vector_store = vector_store
+        self.embedder = embedder
+
+    def retrieve(self, question: str, top_k: int = 5) -> list[str]:
+        query_vector = self.embedder.embed_query(question)
+
+        points = self.vector_store.search(
+            query_vector=query_vector,
+            limit=top_k
+        )
+        
+        contexts = []
+
+        for p in points:
+            if isinstance(p, tuple):
+                for item in p:
+                    if isinstance(item, dict) and "text" in item:
+                        contexts.append(item["text"])
+                        break
+
+        return contexts
+
+        
+        # print(results)
+        # print(type(results[0]), len(results[0]))
+
+        # contexts = [ p.payload["text"] for p in points if p.payload and "text" in p.payload]
+
+        # return contexts
+
+# class RetrievalService:
+#     def __init__(
+#         self,
+#         embedder: Embedder,
+#         vector_store: VectorStore,
+#     ):
+#         self.embedder = embedder
+#         self.vector_store = vector_store
+
+#     def retrieve(self, question: str, top_k: int) -> List[str]:
+#         query_vector = self.embedder.embed([question])[0]
+
+#         results = self.vector_store.search(
+#             query_vector=query_vector,
+#             limit=top_k,
+#         )
+
+#         return [
+#             hit.payload["text"]
+#             for hit in results
+#             if "text" in hit.payload
+#         ]
