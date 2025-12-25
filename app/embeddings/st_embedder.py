84f222998b7072e846834079eb6e6aa88fb7c082
sachin-source <sachinmgvt@gmail.com>
2025-12-25 14:13:48 +0530

    Implement embedder class

--- app/embeddings/st_embedder.py
+++ app/embeddings/st_embedder.py
@@ -9,10 +9,16 @@ class SentenceTransformerEmbedder(Embedder):
     def __init__(self, model_name: str = "BAAI/bge-small-en-v1.5"):
         self.model = SentenceTransformer(model_name)
 
-    def embed(self, texts: List[str]) -> List[list[float]]:
-        embeddings = self.model.encode(
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return self.model.encode(
             texts,
             normalize_embeddings=True,
             show_progress_bar=False,
-        )
-        return embeddings.tolist()
+        ).tolist()
+
+    def embed_query(self, text: str) -> List[float]:
+        return self.model.encode(
+            text,
+            normalize_embeddings=True,
+            show_progress_bar=False,
+        ).tolist()
